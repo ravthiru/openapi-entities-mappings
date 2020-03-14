778d769570f8ae0858e7500af35e68401b5ce47e
Ravi <ravthiru@users.noreply.github.com>
2020-03-14 18:30:44 +1100

    open api spec

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM adoptopenjdk/openjdk11:alpine-jre
+VOLUME /tmp
+EXPOSE 8080
+COPY target/openapi-entities-mappings-1.0.0.jar app.jar
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
---
+++ src/main/java/com/github/ravthiru/openapi/OpenAPIBeansToEntititesMappingApplication.java
@@ -0,0 +1,13 @@
+package com.github.ravthiru.openapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAPIBeansToEntititesMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenAPIBeansToEntititesMappingApplication.class, args);
+	}
+
+}
